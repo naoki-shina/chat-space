02d7caef09a88d585df356935b32f7191ee38450
Naoki Shinagawa <naoki.shina@gmail.com>
2018-02-14 13:04:09 +0900

    add route about group

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
   resource :user, only: [:edit, :update]
+  resource :group, only: [:new, :create, :edit, :update]
+  root "messages#index"
 end

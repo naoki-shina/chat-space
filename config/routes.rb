2328ebfd4559c77e141519d5068d74144e5b500a
Naoki Shinagawa <naoki.shina@gmail.com>
2018-02-16 21:43:49 +0900

    make route path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resource :users, only: [:edit, :update]
-  resource :groups, only: [:new, :create, :edit, :update] do
-    resource :messages, only: [:index, :create]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
   end
-  root "messages#index"
+  root "groups#index"
+
 end

8a9f5cfabe140c80abd78b8e69c8ca9e924c3661
Naoki Shinagawa <naoki.shina@gmail.com>
2018-02-26 19:10:33 +0900

    remove class when you choose one member

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -8,13 +8,19 @@ $(function() {
     </div>`
     list_name.append(html);
   }
-
   function appendNoUser(user) {
     var html = `<div class="chat-group-user clearfix">
     <p class="chat-group-user__name">${user}</p>
     </div>`
   }
-
+  function appendAddUser(id, name) {
+    var html =`<div class="chat-group-user clearfix js-chat-memer" id="chat-group-user-8">
+    <input name="group[user_ids][]" type="hidden" value=${id}>
+    <p class="chat-group-user__name">${name}</p>
+    <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>
+    </div>`
+    list_name.append(html);
+  }
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
     $.ajax({
@@ -38,4 +44,17 @@ $(function() {
       alert("error");
     });
   });
+  $('.chat-group-user').on('click', '.chat-group-user__btn--add', function() {
+    var id = $(this).data('user-id');
+    var name = $(this).data('user-name');
+    $(this).parent().remove();
+    appendAddUser(id, name);
+  });
+  $('.chat-group-user').on('click', '.js-remove-btn', function() {
+    $(this).parent().remove();
+  });
 });
+
+
+
+

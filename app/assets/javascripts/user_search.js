2e4e47d6d00f61531eeeaa805d57f7bfe94b5661
Naoki Shinagawa <naoki.shina@gmail.com>
2018-02-26 20:28:57 +0900

    adjustment position about chat-group-user__btn

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -2,7 +2,7 @@ $(function() {
   var list_name = $('.chat-group-user__name')
 
   function appendUser(user) {
-    var html = `<div class="chat-group-user clearfix">
+    var html = `<div class="chat-group-user chat-group-user__line clearfix">
     <p class="chat-group-user__name">${user.name}</p>
     <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
     </div>`

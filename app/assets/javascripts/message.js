9b2cea9e4de98db393ad416bcaa5a8861709b182
Naoki Shinagawa <naoki.shina@gmail.com>
2018-03-01 21:31:04 +0900

    add setinterval n ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -34,10 +34,45 @@ $(function() {
       $('.contents__main__ajax').append(html);
       $('.textbox').val('');
       $('#textImage').val('');
+      $('.send--button').prop('disabled', false);
       $('html,body').animate({scrollTop: $('.contents__main__ajax').height()}, 500, 'swing');
     })
     .fail(function(){
       alert('error');
     });
+  });
+
+
+
+  function update() {
+    var message_id = $('.messages:last').data('id');
+    $.ajax({
+      url: location.href,
+      type: 'GET',
+      data: {
+        message: { id: message_id }
+      },
+      dataType: 'json'
+    })
+    .done(function(data) {
+      $.each(data, function(i, data) {
+        var html = buildHTML(data);
+        $('.contents__main__ajax').append(html);
+        $('html,body').animate({scrollTop: $('.contents__main__ajax').height()}, 500, 'swing');
+      });
+    });
+    if($('.messages')[0]){
+      var message_id = $('.messages:last').data('id');
+    } else {
+      var message_id = 0
+    }
+  }
+
+  $(function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+      var interval = setInterval(update, 50000);
+    } else {
+      clearInterval(interval)
+    };
   });
 });
